7c1674b5d365541045f81fe166e26233eb428ab9
heytroys <150818764+heytroys@users.noreply.github.com>
2023-12-30 18:08:40 +0900

    Update touitu.js

--- assets/js/touitu.js
+++ assets/js/touitu.js
@@ -8,7 +8,7 @@
   <div class="container-fluid">
     <h1><a href="index.html">Shotaro Sports</a></h1>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"><i class="bi bi-list"></i></span>
+      <span><i class="bi bi-list"></i></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
