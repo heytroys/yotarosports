195efb3cbf76db6f3ad429928f676c952edcbe10
heytroys <150818764+heytroys@users.noreply.github.com>
2023-12-30 21:19:53 +0900

    Update scroll.js

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,5 +1,5 @@
 const move = function() {
-    const target = document.getElementsByClassName("img");
+    const target = document.getElementsByClassName("img", "mae");
 
     const position = Math.floor(window.innerHeight * 1.30);
 
@@ -7,23 +7,9 @@ const move = function() {
         let offsetTop = Math.floor(target[i].getBoundingClientRect().top);
 
         if (offsetTop < position) {
-            target[i].classList.add("animate__animated", "animate__zoomInLeft", "img-after");
-        }
-    }
-}
-
-const fadeUp = function() {
-    let target = document.getElementsByClassName("mae");
-    let position = Math.floor(window.innerHeight * 1.30);
-
-    for (let i = 0; i < target.length; i++) {
-        let offsetTop = Math.floor(target[i].getBoundingClientRect().top);
-
-        if (offsetTop < position) {
-            target[i].classList.add("ato");
+            target[i].classList.add("animate__animated", "animate__zoomInLeft", "img-after","ato");
         }
     }
 }
 
 window.addEventListener("scroll", move, false);
-window.addEventListener("scroll", fadeUp, false);
